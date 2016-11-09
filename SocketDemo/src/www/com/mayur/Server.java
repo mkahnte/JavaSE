62b60d2326f85ad03c6715d5b022514fc14a17e7
mkhante <m.u.khante@gmail.com>
2016-11-09 16:05:52 +0530

    Server and Clinet are Connected , to write the logic of data fetch

---
+++ SocketDemo/src/www/com/mayur/Server.java
@@ -0,0 +1,90 @@
+package www.com.mayur;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+
+public class Server
+{
+	JFrame frame;
+	JButton btnSend;
+	JTextArea txtAreaServer;
+	ServerSocket serverSocket;
+	Socket server_Socket;
+	DataInputStream in;
+	DataOutputStream out; 
+
+		
+	public Server()
+	{
+		frame = new JFrame(" Server ");
+		btnSend = new JButton(" Send Data To Client  ");
+		txtAreaServer = new JTextArea();
+		
+		frame.setLayout(new GridLayout(2,1));
+		btnSend.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				sendDatatoClient();
+			}
+		});
+		
+		
+		frame.getContentPane().add(txtAreaServer);
+		frame.getContentPane().add(btnSend);
+				
+		frame.setSize(300, 400);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		
+	}
+	
+	public static void main(String [] args)
+	{
+		
+		Server s = new Server();
+		s.createConnection(4000);
+	
+		
+	}
+	
+	public void createConnection(int port)
+	{
+		try
+		{
+			serverSocket  = new ServerSocket(port);
+			
+			server_Socket = serverSocket.accept();
+			
+			in = new DataInputStream(server_Socket.getInputStream());
+			out = new DataOutputStream(server_Socket.getOutputStream());
+			
+		}
+		catch (IOException e)
+		{
+			System.out.println(" Error In Connection : "+e.getMessage());
+		}
+	}
+	
+	
+	private void sendDatatoClient()
+	{
+		
+	}
+
+	
+	
+}
