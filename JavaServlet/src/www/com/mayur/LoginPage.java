2d743cb8572d22c07c466459daf190fe94b9bdde
mkhante <m.u.khante@gmail.com>
2016-11-12 13:49:39 +0530

    Authentication Code

---
+++ JavaServlet/src/www/com/mayur/LoginPage.java
@@ -0,0 +1,64 @@
+package www.com.mayur;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginPage extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	String page_HTML = "<!DOCTYPE html>"+
+			"<html>"+
+			"<head>"+
+			    "<title> Servlet Page </title>"+
+			"</head>"+
+			"<body>"+
+
+			    "<form action='Login' method='post'>"+
+			    "<span> Username </span> <input type='text' name='txtUsername'/>"+
+			    "<span> Password </span> <input type='text' name='txtPassword'/>"+
+			    "<input type='submit' name ='btnSubmit' value=' SUBMIT ' />"+
+			    "</form>"+
+
+			"</body>"+
+			"</html>";
+	
+	public LoginPage()
+	{
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		String username = request.getParameter("txtUsername");
+		PrintWriter out = response.getWriter();
+
+		if( username==null || username.equals("") )
+		{
+			out.write(page_HTML);
+		}
+		else
+		{
+			out.write(" Working On it");
+		}
+		
+		
+	}
+
+
+	
+}
