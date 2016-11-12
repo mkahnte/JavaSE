2b8dad64f6a0435acb7745d67298fbf7565c82fc
dac <dac@mba73.dypm.org>
2016-11-12 23:36:23 +0530

    Oracle and MySql Connectivity

--- Connnection_JDBC/src/com/org/mayur/OracleDataConnection.java
+++ Connnection_JDBC/src/com/org/mayur/OracleDataConnection.java
@@ -13,11 +13,13 @@ public class OracleDataConnection
 		try
 		{
 			
-			Class.forName("oracle.jdbc.driver.OracleDriver");  
-			
+			  Class.forName("oracle.jdbc.driver.OracleDriver");  
+			//Class.forName("com.mysql.jdbc.Driver");
+
 			Connection con=DriverManager.getConnection(  
 			"jdbc:oracle:thin:@sdc:1521/oracle","user24","user24");  
-							
+		  //"jdbc:mysql://localhost:3306/iacsd","root","root");
+			
 			
 			String query = "select * from emp";
 			
