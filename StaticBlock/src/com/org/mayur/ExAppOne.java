872d82ce4bd032c081c73b7dad48d78f5be6eee0
dac <dac@mba73.dypm.org>
2016-11-19 23:39:53 +0530

    Static Block

---
+++ StaticBlock/src/com/org/mayur/ExAppOne.java
@@ -0,0 +1,29 @@
+package com.org.mayur;
+
+public class ExAppOne
+{
+	
+	public ExAppOne()
+	{
+		System.out.println(" In Constuctor [ExAppOne] ");
+	}
+	
+	static
+	{
+		System.out.println(" In Static Block [STATC] ");
+
+	}
+	
+	public void print()
+	{
+		System.out.println(" In print method [PRINT] ");
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println(" In main [MAIN]");
+
+	}
+
+}
---
+++ StaticBlock/src/com/org/mayur/MainClass.java
@@ -0,0 +1,14 @@
+package com.org.mayur;
+
+public class MainClass
+{
+
+	public static void main(String[] args)
+	{
+			ExAppOne obj =new ExAppOne();
+			
+			//obj.print();
+				
+	}
+
+}
